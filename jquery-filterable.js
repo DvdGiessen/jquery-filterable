473d15f3ae0458f5f09e203a15a3984d1ca7ad91
Daniël van de Giessen <dvdgiessen@gmail.com>
2013-08-04 13:13:44 +0200

    Update jquery-filterable.js

--- jquery-filterable.js
+++ jquery-filterable.js
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013 Daniël van de Giessen
+ * http://www.dvdgiessen.nl/
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
